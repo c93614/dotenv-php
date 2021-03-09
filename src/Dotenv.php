ad30ca22f4b8ad576014c8a25bfc56878562b01a
Robin <186645+c93614@users.noreply.github.com>
2021-03-09 20:47:35 +0800

    init

---
+++ src/Dotenv.php
@@ -0,0 +1,18 @@
+<?php
+namespace Amumu\Dotenv;
+
+class Dotenv {
+    public static $data;
+
+    protected $path;
+    protected $filename;
+
+    public function __construct($path, $filename = '.env.php') {
+        $this->path = $path;
+        $this->filename = $filename;
+    }
+
+    public function load() {
+        self::$data = require($this->path . DIRECTORY_SEPARATOR . $this->filename);
+    }
+}
